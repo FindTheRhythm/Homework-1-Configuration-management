933d4307c28eae1cd6e58571e01624b4ae3c88ef
Данила FindTheRhythm <140545017+SamurayGIF@users.noreply.github.com>
2024-11-23 23:14:51 +0300

    Create test_du.py

---
+++ target_1/tests/test_du.py
@@ -0,0 +1,13 @@
+import unittest
+import os
+import sys
+import tempfile
+import shutil
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+from shell_commands import Shell
+from utils.logger import Logger
+
+class TestDU(unittest.TestCase):
+
+if __name__ == "__main__":
+    unittest.main()
