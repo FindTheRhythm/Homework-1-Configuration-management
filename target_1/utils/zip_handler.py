b8608c0f3f1bef4914093e7a0fa2c8707a736209
Данила FindTheRhythm <140545017+SamurayGIF@users.noreply.github.com>
2024-11-23 23:02:56 +0300

    Update zip_handler.py

--- target_1/utils/zip_handler.py
+++ target_1/utils/zip_handler.py
@@ -1,1 +1,18 @@
+import os
+import zipfile
 
+
+def extract_zip(zip_path, extract_to="vfs_temp"):
+    """
+    Распаковывает архив zip в указанную директорию.
+    Если директория уже существует, она сначала очищается.
+    """
+    if not zipfile.is_zipfile(zip_path):
+        
+    # Удаляем предыдущую директорию, если она существует
+    if os.path.exists(extract_to):
+       
+    # Распаковка архива
+    with zipfile.ZipFile(zip_path, "r") as zip_ref:
+        zip_ref.extractall(extract_to)
+    return os.path.abspath(extract_to)
