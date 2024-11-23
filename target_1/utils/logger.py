cbe372f1a985d5f5de64529f70252c634f5821a8
Данила FindTheRhythm <140545017+SamurayGIF@users.noreply.github.com>
2024-11-23 23:05:15 +0300

    Final logger.py

--- target_1/utils/logger.py
+++ target_1/utils/logger.py
@@ -3,10 +3,17 @@ from datetime import datetime
 
 class Logger:
     def __init__(self, logfile):
-      
+        self.logfile = logfile
+        self.logs = []
 
     def log(self, command, result):
+        entry = {
+            "command": command,
+            "result": result,
+            "timestamp": datetime.now().isoformat()
+        }
+        self.logs.append(entry)
         
-
     def save(self):
-        
+        with open(self.logfile, "w") as log_file:
+            json.dump(self.logs, log_file, indent=4)
