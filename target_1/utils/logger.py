67a39ad81caa2e55962f93f58b83d5e49dd4d0f0
Данила FindTheRhythm <140545017+SamurayGIF@users.noreply.github.com>
2024-11-23 23:02:09 +0300

    Update logger.py

--- target_1/utils/logger.py
+++ target_1/utils/logger.py
@@ -1,1 +1,12 @@
+import json
+from datetime import datetime
 
+class Logger:
+    def __init__(self, logfile):
+      
+
+    def log(self, command, result):
+        
+
+    def save(self):
+        
