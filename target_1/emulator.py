9f223deef953b3f8ccbb12a70832c9916f6b518f
Данила FindTheRhythm <140545017+SamurayGIF@users.noreply.github.com>
2024-11-23 22:42:34 +0300

    Update emulator.py

--- target_1/emulator.py
+++ target_1/emulator.py
@@ -1,1 +1,23 @@
+import argparse
+import os
+import json
 
+
+
+def main():
+    # Инициализация аргументов командной строки
+    parser = argparse.ArgumentParser(description="Эмулятор shell.")
+    parser.add_argument("--hostname", required=True, help="Имя компьютера.")
+    parser.add_argument("--vfs", required=True, help="Путь к архиву виртуальной файловой системы (zip).")
+    parser.add_argument("--logfile", required=True, help="Путь к лог-файлу (json).")
+    parser.add_argument("--script", required=False, help="Путь к стартовому скрипту.")
+    args = parser.parse_args()
+
+    # Распаковка архива VFS
+    vfs_path = extract_zip(args.vfs)
+
+    # Инициализация логгера
+    logger = Logger(args.logfile)
+
+    # Инициализация оболочки
+    shell = Shell(hostname=args.hostname, vfs_path=vfs_path, logger=logger)
