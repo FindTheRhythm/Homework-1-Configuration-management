730c7d3be73b28dd61e472e1a47cc8530aed4cf7
Данила FindTheRhythm <140545017+SamurayGIF@users.noreply.github.com>
2024-11-23 23:22:03 +0300

    Final test_whoami.py

--- target_1/tests/test_whoami.py
+++ target_1/tests/test_whoami.py
@@ -6,6 +6,27 @@ from shell_commands import Shell
 from utils.logger import Logger
 
 class TestWhoami(unittest.TestCase):
+    def setUp(self):
+        self.logger = Logger("test_log.json")
+        self.test_dir = os.path.abspath("test_vfs")
+        os.makedirs(self.test_dir, exist_ok=True)
+        self.shell = Shell("testhost", self.test_dir, self.logger)
+
+    def tearDown(self):
+        # Удаляем тестовую директорию
+        for root, dirs, files in os.walk(self.test_dir, topdown=False):
+            for name in files:
+                os.remove(os.path.join(root, name))
+            for name in dirs:
+                os.rmdir(os.path.join(root, name))
+        os.rmdir(self.test_dir)
+
+    def test_whoami_output(self):
+        import getpass
+        expected_user = getpass.getuser()
+        self.shell.whoami()
+        last_log = self.logger.logs[-1]
+        self.assertEqual(last_log["result"], expected_user)
 
 if __name__ == "__main__":
     unittest.main()
