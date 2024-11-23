0896e7ae88c27fff72dfa94b91014f4e5e25be19
Данила FindTheRhythm <140545017+SamurayGIF@users.noreply.github.com>
2024-11-23 23:03:52 +0300

    Final zip_handler.py

--- target_1/utils/zip_handler.py
+++ target_1/utils/zip_handler.py
@@ -8,10 +8,16 @@ def extract_zip(zip_path, extract_to="vfs_temp"):
     Если директория уже существует, она сначала очищается.
     """
     if not zipfile.is_zipfile(zip_path):
+        raise ValueError("Указанный файл не является zip-архивом.")
         
     # Удаляем предыдущую директорию, если она существует
     if os.path.exists(extract_to):
-       
+        for root, dirs, files in os.walk(extract_to, topdown=False):
+            for name in files:
+                os.remove(os.path.join(root, name))
+            for name in dirs:
+                os.rmdir(os.path.join(root, name))
+                
     # Распаковка архива
     with zipfile.ZipFile(zip_path, "r") as zip_ref:
         zip_ref.extractall(extract_to)
