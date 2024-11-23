bd2974a5fbcaad2ccf39e4311b9b7d13565aa280
Данила FindTheRhythm <140545017+SamurayGIF@users.noreply.github.com>
2024-11-23 23:20:38 +0300

    Final test_ls.py

--- target_1/tests/test_ls.py
+++ target_1/tests/test_ls.py
@@ -6,6 +6,14 @@ from shell_commands import Shell
 from utils.logger import Logger
 
 class TestLS(unittest.TestCase):
+    def setUp(self):
+        self.logger = Logger("test_log.json")
+        self.shell = Shell("testhost", os.getcwd(), self.logger)
+
+    def test_ls_current_dir(self):
+        result = self.shell.ls()
+        self.assertTrue(result)
+        self.assertIn("shell_commands.py", os.listdir())
 
 if __name__ == "__main__":
     unittest.main()
