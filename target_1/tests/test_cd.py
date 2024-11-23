40fe89509e0e27dd1300b991e2b7a4dbaa5819ea
Данила FindTheRhythm <140545017+SamurayGIF@users.noreply.github.com>
2024-11-23 23:14:12 +0300

    Create test_cd.py

---
+++ target_1/tests/test_cd.py
@@ -0,0 +1,11 @@
+import unittest
+import os
+import sys
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+from shell_commands import Shell
+from utils.logger import Logger
+
+class TestCD(unittest.TestCase):
+
+if __name__ == "__main__":
+    unittest.main()
